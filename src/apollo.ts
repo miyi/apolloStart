be805d9bbd1b92e85e30c4713705b5e22cb0dcef
miyi <mylegojesus@gmail.com>
2020-05-01 19:07:01 -0700

    everything works right now I don't know why

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from 'express'
 import { ApolloServer, gql, IResolvers } from 'apollo-server-express'
+// import cors from 'cors'
+import session from 'express-session'
+import { createClient } from 'redis'
+import connectRedis from 'connect-redis'
+
+const SESSION_SECRET = 'secretsecret'
 
 interface AddressInfo {
   address: string
@@ -17,6 +23,13 @@ const typeDefs = gql`
   type Query {
     hello: String
     url: String
+    readDummy1: String
+    readDummy2: String
+  }
+
+  type Mutation {
+    setDummy1: String
+    setDummy2: String
   }
 `
 
@@ -27,20 +40,59 @@ const resolvers: IResolvers = {
     url: (_: void, __: void, context: any) => {
       return context.url
     },
+    readDummy1: (_: void, __: void, context: any) => {
+      return context.session.req.session.dummy1
+    },
+    readDummy2: (_: void, __: void, context: any) => {
+      return context.session.req.session.dummy2
+    },
   },
+  Mutation: {
+    setDummy1: (_: void, __: void, context: any) => {
+      context.session.req.session.dummy1 = true
+      return "dummy1 set"
+    },
+    setDummy2: (_: void, __: void, context: any) => {
+      context.session.req.session.dummy2 = true
+      return "dummy2 set"
+    }
+  }
 }
 
 const graphqlServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }: ContextIntegration) => ({
-    url: req.host + '://' + req.get('host'),
+  context: ({ req, res }: ContextIntegration) => ({
+    url: req.get('host'),
+    session: {req, res}
   }),
 })
 
-const app = express()
-graphqlServer.applyMiddleware({ app, path: '/graphql' })
+const RedisClient = createClient()
+const RedisStore = connectRedis(session)
 
-let server = app.listen(3000, 'localhost', () => {
-  console.log('Listening on port: ', server.address() as AddressInfo)
+const app = express()
+app.use(
+  session({
+    store: new RedisStore({
+      client: RedisClient as any,
+    }),
+    name: 'qid',
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    },
+  }),
+)
+app.set('trust proxy', 1)
+const corsOptions = { credentials: true, origin: '*', }
+graphqlServer.applyMiddleware({ app, cors: corsOptions, path: '/graphql' })
+
+let httpServer = app.listen(3000, 'localhost', () => {
+  console.log('Listening on port: ', httpServer.address() as AddressInfo)
 })
